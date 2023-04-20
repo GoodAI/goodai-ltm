a213e19385c90f6c06a9ff68322cb56d5f3d748c
Jose Solorzano <jose.solorzano@goodai.com>
2023-04-20 16:15:25 -0500

    max_scenarios parameter in factory

--- goodai/ltm/eval/auto.py
+++ goodai/ltm/eval/auto.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from transformers import PreTrainedTokenizer
 
@@ -13,10 +13,14 @@ class AutoMemEvaluator:
 
     @staticmethod
     def create(name: str, tokenizer: PreTrainedTokenizer, top_ks: List[int], max_query_tokens: int,
-               has_query_noise: bool):
+               has_query_noise: bool, max_scenarios: Optional[int] = None):
+        if max_scenarios is None:
+            max_scenarios = 2000
         if name == 'qrecc':
-            return QreccMemEvaluator(tokenizer, top_ks, max_query_tokens, has_query_noise)
+            return QreccMemEvaluator(tokenizer, top_ks, max_query_tokens, has_query_noise,
+                                     max_scenarios=max_scenarios)
         elif name == 'strategyqa':
-            return StrategyQAMemEvaluator(tokenizer, top_ks, max_query_tokens, has_query_noise)
+            return StrategyQAMemEvaluator(tokenizer, top_ks, max_query_tokens, has_query_noise,
+                                          max_scenarios=max_scenarios)
         else:
             raise ValueError(f'Dataset name "{name}" not recognized')
