3083f17ee9aa24ee2432e52b348edb8432a00726
Jose Solorzano <jose.solorzano@goodai.com>
2023-04-24 11:38:45 -0500

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ packages = find_namespace_packages(include=['goodai.*'])
 setup(name='goodai-ltm',
       description='A text memory meant to be used with conversational language models.',
       url='https://github.com/GoodAI/goodai-ltm',
-      version='0.0.12',
+      version='0.0.13',
       packages=packages,
       package_data={'goodai.ltm.data': ['**/*.json']},
       install_requires=['torch>=1.8.0', 'pytest>=7.0.0', 'numpy>=1.19.0', 'transformers>=4.0.0',
